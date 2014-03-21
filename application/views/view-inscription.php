300ee17c4a77de49cbe9dcf5ed86c087b7d4dfb6
GroupeM2J <jb.blois@iia-laval.fr>
2014-03-21 12:27:47 +0100

    ADD barre de navigation

--- application/views/view-inscription.php
+++ application/views/view-inscription.php
@@ -4,8 +4,20 @@
         <title>Inscription - MegaCasting -</title>
     </head>
     <body>
-     
-        
+         
+        <div class="navbar">
+        <div class="navbar-inner">
+        <ul class="nav">
+        <li><a href="accueil.php">Accueil</a></li>
+        <li><a href="listeOffres.php">Les Offres</a></li>
+        <li><a href="partenaires.php">Nos Partenaires</a></li>
+        <li><a href="description.php">Qui sommes nous?</a></li>
+        <li><a href="contact.php">Nous contacter</a></li>
+        <li><a href="connexion.php">Se connecter</a></li>
+        </ul>
+        </div>
+        </div>
+
          <h1>Formulaire d'inscription :  </h1>
         
          <form method="post" action="" enctype="multipart/form-data"><div>
